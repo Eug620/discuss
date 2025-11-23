431031cb741971c3c239f32ccd0bc59a2cdf1cc9
Eug <yyh3531@163.com>
2025-11-23 21:23:17 +0800

    update

--- src/layout/_index.vue
+++ src/layout/_index.vue
@@ -6,17 +6,17 @@
           v-for="page in initRoutes"
           @click="handleClick(page)"
           :key="page.path"
-          class="cursor-pointer text-center p-2"
+          class="cursor-pointer text-center p-2 hover:bg-gray-300"
         >
-        <div class="w-6 py-1 mx-auto"  v-html="page.meta.icon"></div>
+        <div class="w-6 py-1 mx-auto text-slate-600"  v-html="page.meta.icon"></div>
       </div>
       </div>
       <div class="">
         <div
-          class="cursor-pointer text-center p-2"
+          class="cursor-pointer text-center p-2 hover:bg-gray-300"
           @click="handleLogout"
         >
-        <div class="w-6 py-1 mx-auto " >
+        <div class="w-6 py-1 mx-auto text-slate-600 " >
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
             <path stroke-linecap="round" stroke-linejoin="round" d="M9 15 3 9m0 0 6-6M3 9h12a6 6 0 0 1 0 12h-3" />
           </svg>
