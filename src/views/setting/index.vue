c088ee5ea0bdaa90f09dbb651b16799be1184c38
Administrator <418463157@qq.com>
2025-12-27 10:42:05 +0800

    update

--- src/views/setting/index.vue
+++ src/views/setting/index.vue
@@ -1,9 +1,9 @@
 <template>
-    <div class="w-full text-sm flex items-center justify-center h-full">
-        <div class="w-1/2">
-            <div v-for="(item, k) in userStore.userInfo" :key="k" class="flex gap-4">
-                <div class="w-[120px] text-right">{{ k }}:</div>
-                <div class="flex-1 w-full auto-wrap">{{ item }}</div>
+    <div class="w-full text-sm flex items-center justify-center h-full relative">
+        <div class="w-1/2 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+            <div v-for="(item, k) in userStore.userInfo" :key="k" class="flex">
+                <div class="w-[120px] text-right outline outline-1 outline-gray-300 p-1 pr-2">{{ k }}:</div>
+                <div class="flex-1 w-full auto-wrap outline outline-1 outline-gray-300 p-1 pl-2">{{ item }}</div>
             </div>
         </div>
     </div>
