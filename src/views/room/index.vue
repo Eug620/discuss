53f087da1f04cf89957adbb36c86253c9616d359
yeyuhang <yeyuhang@zzltsw.com>
2026-03-13 16:10:19 +0800

    feat(room): user

--- src/views/room/index.vue
+++ src/views/room/index.vue
@@ -75,22 +75,21 @@
           <div class="flex-1">
               <div class="border-b border-gray-300 p-2 pl-0">群成员</div>
               <div v-for="member in getMember" :key="member.user_id"
-                  class="text-sm flex justify-between items-center py-1" :class="{
-                      'underline underline-offset-2': member.user_id === userStore.userInfo.id,
-                  }">
-                  
-
-                  <span class="flex gap-1 items-center" :class="{
+                  class="text-sm flex gap-1 items-center py-1" :class="{
+                    'underline underline-offset-2': member.user_id === userStore.userInfo.id,
                     'text-green-700': getMemberOnline.has(member.user_id)
                   }">
-                    <!-- <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
-                      <path stroke-linecap="round" stroke-linejoin="round" d="M18 18.72a9.094 9.094 0 0 0 3.741-.479 3 3 0 0 0-4.682-2.72m.94 3.198.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0 1 12 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 0 1 6 18.719m12 0a5.971 5.971 0 0 0-.941-3.197m0 0A5.995 5.995 0 0 0 12 12.75a5.995 5.995 0 0 0-5.058 2.772m0 0a3 3 0 0 0-4.681 2.72 8.986 8.986 0 0 0 3.74.477m.94-3.197a5.971 5.971 0 0 0-.94 3.197M15 6.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm6 3a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Zm-13.5 0a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Z" />
-                    </svg>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
-                      <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
-                    </svg> -->
-                    {{ member.user_info.username }}
-                  </span>
+                  <!-- 群主 -->
+                  <svg v-if="getRoomInfo.creator === member.user_id" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z" />
+                  </svg>
+
+                  <!-- 成员 -->
+                  <svg v-else xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+                  </svg>
+
+                  <span class="flex gap-1 items-center">{{ member.user_info.username }}</span>
               </div>
           </div>
           <div class="flex-1" v-if="applyList.length > 0">
